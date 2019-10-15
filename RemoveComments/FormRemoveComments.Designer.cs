4e7274cd51406d30ed01e1d7fb5aef3311845d18
Edson de Oliveira Júnior <edsonoliveirajr@outlook.com>
2019-10-15 01:18:35 -0300

    Initial commit

---
+++ RemoveComments/FormRemoveComments.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace RemoveComments
+{
+    partial class FormRemoveComments
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(430, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(461, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(556, 95);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ RemoveComments/FormRemoveComments.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RemoveComments
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string folder = textBox1.Text;
+
+            RemoveCommentsFromFolder(folder);
+        }
+
+        /// <summary>
+        /// Substitui os nomes do arquivos da pasta
+        /// </summary>
+        /// <param name="folder"></param>
+        /// <param name="oldString"></param>
+        /// <param name="newString"></param>
+        /// <param name="mainFolder"></param>
+        private void RemoveCommentsFromFolder(string folder)
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(folder);
+
+            var files = directoryInfo.GetFiles();
+
+            RemoveCommentsFromFiles(files);
+
+            var folders = directoryInfo.GetDirectories();
+
+            foreach (var subFolder in folders)
+            {
+                RemoveCommentsFromFolder(subFolder.FullName);
+            }
+        }
+
+        private static void RemoveCommentsFromFiles(FileInfo[] files)
+        {
+            string[] extensions = { ".js", ".css", ".cshtml", ".cs" };
+
+            foreach (var extension in extensions)
+            {
+                foreach (FileInfo file in files.Where(x => x.FullName.EndsWith(extension)))
+                {
+                    RemoveComments(file);
+                }
+            }
+        }
+
+        private static void RemoveComments(FileInfo file)
+        {
+            var lines = new List<string>(File.ReadAllLines(file.FullName));
+
+            if (lines.Count > 0)
+            {
+                while ((lines.Count > 0) && (lines.First()?.Trim() == String.Empty))
+                {
+                    lines.RemoveAt(0);
+                }
+
+                lines.RemoveAll(
+                    x => (x.Contains("//")) ||
+                         (x.Contains("@*")) ||
+                         (x.Contains("< !--")) ||
+                         (x.StartsWith("/*")) ||
+                         (x.StartsWith(" *")));
+
+                file.IsReadOnly = false;
+                File.WriteAllLines(file.FullName, lines);
+            }
+        }
+    }
+}
