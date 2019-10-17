540b96824d329e30c53eb1140bd719b710ce4ead
edsoliveirajr <edson.oliveira@NOVASOLUCOES.LOCAL>
2019-10-17 16:08:28 -0300

    rename form

--- RemoveComments/FormRemoveComments.cs
+++ RemoveComments/FormRemoveComments.cs
@@ -11,9 +11,9 @@ using System.Windows.Forms;
 
 namespace RemoveComments
 {
-    public partial class Form1 : Form
+    public partial class FormRemoveComments : Form
     {
-        public Form1()
+        public FormRemoveComments()
         {
             InitializeComponent();
         }
--- RemoveComments/Program.cs
+++ RemoveComments/Program.cs
@@ -16,7 +16,7 @@ namespace RemoveComments
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormRemoveComments());
         }
     }
 }
